f605b473f7adc7f1ddbcc0519fc78aab3b7878aa
Palash Chauhan <palashc983@gmail.com>
2017-03-28 21:33:03 +0530

    script to generate predicted labels using 2-step clfrs

--- eval-1/eval.py
+++ eval-1/eval.py
@@ -12,9 +12,7 @@ for score_file in score_files:
 	print score_file
 	name = score_file[7:].replace("-score.txt", "")
 	pred[name] = []
-	#predfile = "pred/" + name.upper() + ".txt"
-	
- 	lines = open(score_file, "r").readlines()
+	lines = open(score_file, "r").readlines()
 	lines = lines[1:]
 	prediction = ""
 	predDict = {}
@@ -24,25 +22,27 @@ for score_file in score_files:
 			score = score.split(":")
 			predDict[int(score[0])] = float(score[1])
 
+		#sort in descending order and pick up top 3
 		newA = dict(sorted(predDict.iteritems(), key=operator.itemgetter(1), reverse=True)[:3])
 		temp = [str(i) for i in newA.keys()]
-		# newA = " ".join(temp)
-		# prediction += newA
-		# prediction += '\n'
 		pred[name].append(temp)
 
-
+	# save to file if needed ######
+	#
+	# predfile = name + "-pred.txt"
 	# f1 = open(predfile, "w")
 	# f1.write(prediction)
 	# f1.close()
 	# print "--> Done!"
+	#
+	#############################
 
 
 #################################################################
 print "creating index 2 word maps...."
-pred_pos = ['in', 'cd', 'jj', 'nn', 'nns', 'nnp', 'vbn', 'vbd', 'vbg', 'vb', 'nnps']
-pkls = glob.glob("../postag_data/*.pkl")
-pklDict = {}
+pred_pos = ['in', 'cd', 'jj', 'nn', 'nns', 'nnp', 'vbn', 'vbd', 'vbg', 'vb', 'nnps']   #the tags which have clfrs trained
+pkls = glob.glob("../postag_data/*.pkl")   ##get this from google drive link of categorized data
+pklDict = {}  
 for pickl in pkls:
 	print pickl
 	name = pickl.replace("../postag_data/", "")[:-10]
@@ -53,11 +53,11 @@ my_map = pickle.load(open("../taggedLabelDict.pkl", "r"))
 taggedLabelDict = {v: k for k, v in my_map.iteritems()}
 finalAns = ''
 
-def getPermutations(l):
+#take top 3 predictions of every pos tag clfr in the predicted meta label and return all possible combinations
+def getPermutations(l):      
 	permuts = list(itertools.product(*l))
 	ans = ''
 	for t in permuts:
-		#print t, type(t)
 		ans += "|"
 		ans += " ".join(t)
 	return ans
@@ -74,21 +74,20 @@ for i in range(len(preds)):
 		symbols = j.split(" ")
 		symbols = [symbol.lower() for symbol in symbols]
 		for s in symbols:
-			if s in ["(", ")", "-", ":", ";", ","]:
+			if s in ["(", ")", "-", ":", ";", ","]:          #to be taken as it is
 				temp.append([s])
-			elif s not in pred_pos:
+			elif s not in pred_pos:                          #no clfr since support<5
 				temp.append(pklDict[s].values())
 			else:
-				#print s, pred[s][i]
 				temp1 = [pklDict[s][int(q)] for q in pred[s][i]]
 				temp.append(temp1)
-		#print temp, len(temp), len(temp[0])
 		perms = getPermutations(temp)
-		#print perms
 		finalPreds += perms
 	finalAns += finalPreds
 	finalAns += '\n'
 
+
+#write to file
 myf = open("predictedLabels.txt","w")
 myf.write(finalAns)
 myf.close()
